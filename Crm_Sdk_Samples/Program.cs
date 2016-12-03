d09e668642dd29338f2a63e6ffc028597f1473e6
Apurv Ghai <apurv.ghai@hotmail.com>
2016-12-03 14:47:31 -0600

    Revert "web api c#"
    This reverts commit 79e48ed5e945aa1f48f00ed10a85740b11cc236d.

--- Crm_Sdk_Samples/Program.cs
+++ Crm_Sdk_Samples/Program.cs
@@ -1,10 +1,4 @@
-﻿using Crm_Sdk_Samples.WebAPI_Samples;
-using Microsoft.Xrm.Sdk;
-using Newtonsoft.Json.Linq;
-using System;
-using System.Net.Http;
-using System.Net.Http.Headers;
-using System.Threading.Tasks;
+﻿using Microsoft.Xrm.Sdk;
 
 namespace Crm_Sdk_Samples
 {
@@ -14,39 +8,12 @@ namespace Crm_Sdk_Samples
         {
             //Running the sample
             CrmToolingHelper toolingHelper = new CrmToolingHelper();
-            IOrganizationService orgServcs = toolingHelper.GetConnectToCrm(CrmAuthType.OAuth);
-
+            IOrganizationService orgServcs = toolingHelper.GetConnectToCrm(CrmAuthType.Office365);
+            
             //Metadaata functions
-            //CrmMetadataHelper crmMetadataHelper = new CrmMetadataHelper();
-            //crmMetadataHelper.DoRetrieveAllEntities(orgServcs);
+            CrmMetadataHelper crmMetadataHelper = new CrmMetadataHelper();
+            crmMetadataHelper.DoRetrieveAllEntities(orgServcs);
 
-            Program app = new Program();
-            Task.WaitAll(Task.Run(async () => await app.CreateMyReordsAsync()));
-
-
-        }
-
-
-        async Task CreateMyReordsAsync()
-        {
-            WebApiOperationHelper apihelper = new WebApiOperationHelper();
-            apihelper.BaseOrganizationApiUrl = "https://org.api.crm.dynamics.com";
-            apihelper.ObtainOAuthToken();
-
-            if (!(string.IsNullOrEmpty(apihelper.AccessToken)))
-            {
-                //Create Sample Contact
-                JObject contact1 = new JObject();
-                contact1.Add("firstname", "Peter");
-                contact1.Add("lastname", "Cambel");
-
-                string filter = "?$select=firstname&$filter=contains(firstname,'Peter')";
-
-                //await apihelper.CreateEntityRecords("contacts", contact1);
-                await apihelper.SearchExistingRecord("contacts", filter);
-                Console.Read();
-            }
-            Console.Read();
         }
     }
 }
