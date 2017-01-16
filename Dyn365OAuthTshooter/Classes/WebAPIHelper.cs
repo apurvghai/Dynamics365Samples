4bfb45d495203e237a6957ad683872a61b2af061
Apurv <apurv.ghai@outlook.com>
2017-01-16 09:12:25 -0600

    WebAPIHelper Modifications to include the API Version WebAPI Troubleshooting Tool

---
+++ Dyn365OAuthTshooter/Classes/WebAPIHelper.cs
@@ -0,0 +1,118 @@
+﻿/*
+        Author : Apurv Ghai
+        Description: The program has been distributed *as is* to help the community members and do not certify to be used for Production Use.
+ */
+
+using Microsoft.IdentityModel.Clients.ActiveDirectory;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+/// <summary>
+/// This helper provides facility to connect to Dynamics 365 using webapi and perform basic CRUD Operations.
+/// </summary>
+namespace Dyn365Samples
+{
+    public class WebAPIHelper
+    {
+        public string AccessToken { get; set; }
+
+        public string BaseOrganizationApiUrl { get; set; }
+
+        //Provides a persistent client-to-CRM server communication channel.
+        HttpClient httpClient = new HttpClient();
+
+        /// <summary>
+        /// This function will return the access token based on authority, ClientId and Dynamics 365 Url (Applies to Online and OnPremise)
+        /// </summary>
+        public void ObtainOAuthToken(string clientId, string redirectStringUrl, AuthenticationParameters authParams)
+        {
+            Uri redirectUrl = new Uri(redirectStringUrl);
+            AuthenticationContext authContext = new AuthenticationContext(authParams.Authority, false);
+            AuthenticationResult result = authContext.AcquireToken(authParams.Resource, clientId, redirectUrl, PromptBehavior.Always);
+            if (result != null)
+                AccessToken = result.AccessToken;
+        }
+
+        /// <summary>
+        /// This function returns the Authority Url (OAuth Endpoint) and Resource Url (Dynamics 365 Url)
+        /// </summary>
+        /// <returns></returns>
+        public AuthenticationParameters DiscoverAuthority()
+        {
+            AuthenticationParameters authParamters = AuthenticationParameters.CreateFromResourceUrlAsync
+                (new Uri(BaseOrganizationApiUrl + "/api/data/")).Result;
+            return authParamters;
+        }
+
+        /// <summary>
+        /// This function can create any record type using Json Entity Objects
+        /// </summary>
+        /// <param name="entityName"></param>
+        /// <param name="entityObject"></param>
+        /// <returns></returns>
+        public async Task CreateEntityRecords(string entityName, JObject entityObject, string apiVersion)
+        {
+            httpClient = CreateDynHttpClient(AccessToken, entityName);
+            HttpRequestMessage createHttpRequest = new HttpRequestMessage(HttpMethod.Post, BaseOrganizationApiUrl + "/api/data/" + apiVersion + "/" + entityName);
+            createHttpRequest.Content = new StringContent(entityObject.ToString(), Encoding.UTF8, "application/json");
+            var response = await httpClient.SendAsync(createHttpRequest);
+            response.EnsureSuccessStatusCode();
+            if (response.StatusCode == HttpStatusCode.NoContent)
+            {
+                // Do something with response. Example get content:
+                Console.WriteLine("The record was created successfully.");
+                Console.ReadKey();
+                //Dispose the Object :: Best Practice
+                httpClient.Dispose();
+            }
+        }
+
+        /// <summary>
+        /// This function can create any record type by providing the Odata filters
+        /// </summary>
+        /// <param name="entityName"></param>
+        /// <param name="filter"></param>
+        /// <returns></returns>
+        public async Task SearchExistingRecord(string entityName, string filter, string apiVersion)
+        {
+            httpClient = CreateDynHttpClient(AccessToken, entityName);
+            string completedFilterCondition = BaseOrganizationApiUrl + "/api/data/" + apiVersion + entityName + filter;
+            var response = await httpClient.GetAsync(completedFilterCondition);
+            response.EnsureSuccessStatusCode();
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                var objParsedContent = JsonConvert.DeserializeObject(content);
+
+                // Do something with response. Example get content:
+                Console.WriteLine(objParsedContent);
+                Console.WriteLine("Records Found");
+                Console.ReadKey();
+                //Dispose the Object :: Best Practice
+                httpClient.Dispose();
+            }
+        }
+
+        private HttpClient CreateDynHttpClient(string accessToken, string apiEntity)
+        {
+
+            HttpClient client = new HttpClient();
+            client.Timeout = TimeSpan.FromSeconds(360);
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+            client.BaseAddress = new Uri(BaseOrganizationApiUrl + "/api/data/v8.1/");
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            client.DefaultRequestHeaders.Add("OData-MaxVersion", "4.0");
+            client.DefaultRequestHeaders.Add("OData-Version", "4.0");
+            client.DefaultRequestHeaders.Add("Cache-Control", "no-cache");
+            return client;
+        }
+    }
+}
---
+++ Dyn365OAuthTshooter/MainWindow.xaml.cs
@@ -0,0 +1,84 @@
+﻿using Dyn365Samples;
+using Microsoft.IdentityModel.Clients.ActiveDirectory;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace Dyn365OAuthTshooter
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public string OrgUrl { get; set; }
+        public string clientId { get; set; }
+        public string redirectUrl { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConnect_Click(object sender, RoutedEventArgs e)
+        {
+            OrgUrl = txtOrgUrl.Text;
+            clientId = txtClientId.Text;
+            redirectUrl = txtRedirectUrl.Text;
+
+            Thread sThread = new Thread(DoWork);
+            sThread.Start();
+        }
+
+        async void DoWork()
+        {
+            await txtOutput.Dispatcher.BeginInvoke(DispatcherPriority.Background, new Action(() => this.txtOutput.Text = string.Empty));
+            await txtOutput.Dispatcher.BeginInvoke(DispatcherPriority.Background, new Action(() => this.txtOutput.Text = "Conecting..."));
+            try
+            {
+                WebAPIHelper apiHelper = new WebAPIHelper();
+                AuthenticationParameters authParams =  apiHelper.DiscoverAuthority();
+                await txtOutput.Dispatcher.BeginInvoke(DispatcherPriority.Background, new Action(() =>
+                {
+                    this.txtoauthEndpoint.Text = authParams.Authority;
+                    this.txtDiscoveryUrl.Text = authParams.Resource;
+                }));
+
+                apiHelper.BaseOrganizationApiUrl = OrgUrl;
+                apiHelper.ObtainOAuthToken(clientId, redirectUrl, authParams);
+                await txtOutput.Dispatcher.BeginInvoke(DispatcherPriority.Background, new Action(() => this.txtOutput.Text = "Finished Obtaining Access Token"));
+                if (!(string.IsNullOrEmpty(apiHelper.AccessToken)))
+                {
+                    await txtOutput.Dispatcher.BeginInvoke(DispatcherPriority.Background, new Action(() => this.txtOutput.Text = "Creating Sample Account in CRM"));
+
+                    //Create Sample Contact
+                    JObject accountJobj = new JObject();
+                    accountJobj.Add("name", "SDK Account");
+                    await apiHelper.CreateEntityRecords("accounts", accountJobj, "v8.1");
+
+                    await txtOutput.Dispatcher.BeginInvoke(DispatcherPriority.Background, new Action(() => this.txtOutput.Text = "Completed Acount Creation"));
+                }
+            }
+            catch (Exception e)
+            {
+                await txtOutput.Dispatcher.BeginInvoke(DispatcherPriority.Background, new Action(() => this.txtOutput.Text = e.Message));
+            }
+
+        }
+
+    }
+}
